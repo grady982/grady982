6b9cb0967380c0d168b4ed0e981bce7b95754508
Grady Liu/WHQ/Wistron <grady_liu@wistron.com>
2022-09-17 14:11:19 +0800

    chore: remove useless import otherwise, eslint will cause compiled error

--- src/components/ResponsiveAppBar/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar/ResponsiveAppBar.tsx
@@ -25,9 +25,9 @@ const ResponsiveAppBar = () => {
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorElUser(event.currentTarget);
+  // };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
