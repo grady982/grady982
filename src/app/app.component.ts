d6b7b93adce7fac3ead1bd54ec175d7099d4edc0
Grady <grady0902@gmail.com>
2025-12-04 19:33:17 +0800

    refactor: app structure by creating individual components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
+import { HeaderComponent } from './../components/header/header.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from '../components/about/about.component';
+import { AchievementsComponent } from '../components/achievements/achievements.component';
+import { SkillsComponent } from '../components/skills/skills.component';
+import { ExperienceComponent } from "../components/experience/experience.component";
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, AboutComponent, AchievementsComponent, SkillsComponent, ExperienceComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'grady982';
